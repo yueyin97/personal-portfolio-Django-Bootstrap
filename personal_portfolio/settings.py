a17cc40bad42317d090ffcaa3d07079fe047277f
mintd <mint.dyy@outlook.com>
2023-03-02 00:59:27 +0000

    migrated to postgres

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = "personal_portfolio.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "portfolio",
+        "USER": "postgres",
+        "PASSWORD": "benbed1997",
+        "PORT": "5432",
+        "HOST": "localhost"
     }
 }
 
